3854e2ebaeeb8d53623e80a815b9d0fa4eb8ba53
SendUser1999 <anastasiya.sendeckaya89@gmail.com>
2024-01-20 15:19:15 +0100

    changes after checking

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,13 +1,11 @@
-// Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
 const galleryList = document.querySelector(".gallery");
 galleryList.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
 
-
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
 
 
 //SimpleLightBox 
@@ -19,13 +17,12 @@ const lightbox = new SimpleLightbox('.gallery a', {
 
 function createMarkup(arr) {
     return arr.map(({ preview, original, description }) => `
-    <li class="gallery__item">
-   <a class="gallery__link" href="${original}">
-      <img class="gallery__image" src="${preview}" alt="${description}" />
+    <li class="gallery-item">
+   <a class="gallery-link" href="${original}">
+      <img class="gallery-image" src="${preview}" alt="${description}" />
    </a>
 </li>
     `).join("")
 }
 
-console.log(galleryItems);
 
